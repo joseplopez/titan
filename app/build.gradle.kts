ed41d9aefdf6bcbb84ae7d09a3833ce6b4a22afe
jple <joseplcatz@gmail.com>
2026-08-18 14:14:40 +0200

    Fixed app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.centelles.titan"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
